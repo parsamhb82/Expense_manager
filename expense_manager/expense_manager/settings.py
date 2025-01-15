87c72ffa6652eb46bf8c3335e54efe9d63583de3
parsamhb82 <parsa1382mohebian@gmail.com>
2025-01-15 17:28:58 +0330

    I dunno

--- expense_manager/expense_manager/settings.py
+++ expense_manager/expense_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["nasty-moments-fold.loca.lt", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["quick-carrots-boil.loca.lt", "127.0.0.1", "localhost"]
 
 
 # Application definition
